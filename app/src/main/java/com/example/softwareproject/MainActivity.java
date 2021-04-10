ab0d369f55fb5bd733a5fc5bc9f913ec4fb6e24f
MichaelWisehart <75412244+MichaelWisehart@users.noreply.github.com>
2021-04-09 17:22:02 -0700

    Hope it works
    first commit i hope it works

---
+++ app/src/main/java/com/example/softwareproject/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.softwareproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.softwareProject.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getBtn;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        getBtn = (Button) findViewById(R.id.getBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite();
+            }
+        });
+    }
+
+    private void getWebsite() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+                    String title = doc.title();
+                    Elements links = doc.select("a[href]");
+
+                    builder.append(title).append("\n");
+
+                    for (Element link : links) {
+                        builder.append("\n").append("Link : ").append(link.attr("href"))
+                                .append("\n").append("Text : ").append(link.text());
+                    }
+                } catch (IOException e) {
+                    builder.append("Error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
+    }
+}
